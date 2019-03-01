48d968f1be7d5df1975a6d52f84bc1e70e894733
Portisch <hugo.portisch@yahoo.de>
2019-03-01 07:19:04 +0100

    added DIO Chacon RF 433Mhz plugs #95

--- inc/RF_Config.h
+++ inc/RF_Config.h
@@ -28,3 +28,4 @@
 #define EFM8BB1_SUPPORT_HT12a_PROTOCOL			1		// HT12A/HT12E chips - Generic Doorbell
 #define EFM8BB1_SUPPORT_HT12_Atag_PROTOCOL		1		// HT12A/HT12E chips - Atag Extractor - Plus/Minus/Lights/Timer
 #define EFM8BB1_SUPPORT_Kaku_PROTOCOL			1		// KaKu wall sockets
+#define EFM8BB1_SUPPORT_DIO_PROTOCOL			1		// DIO Chacon RF 433Mhz, Issue #95
