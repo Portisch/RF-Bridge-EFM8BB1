fd68fe9a78c722aae5e9d2e49905ca39fe760ec4
Portisch <hugo.portisch@yahoo.de>
2019-05-02 09:44:30 +0200

    Add inverted PT2262 remote #116

--- inc/RF_Config.h
+++ inc/RF_Config.h
@@ -14,24 +14,25 @@
 
 // typical protocols, disable here!             Enable	Remarks
 #define EFM8BB1_SUPPORT_PT226X_PROTOCOL			1		// PT2260, EV1527,... original RF bridge protocol
-#define EFM8BB1_SUPPORT_HT6P20X_PROTOCOL		1		// HT6P20X chips
-#define EFM8BB1_SUPPORT_HT12_PROTOCOL			1		// HT12A/HT12E chips
+#define EFM8BB1_SUPPORT_PT2262_PROTOCOL			1		// PT2262, Issue #116
+#define EFM8BB1_SUPPORT_HT6P20X_PROTOCOL		0		// HT6P20X chips
+#define EFM8BB1_SUPPORT_HT12_PROTOCOL			0		// HT12A/HT12E chips
 
 // more protocols, enable here!                 Enable  Remarks
 #define EFM8BB1_SUPPORT_Rohrmotor24_PROTOCOL	0		// Rohrmotor24
 #define EFM8BB1_SUPPORT_PAR56_PROTOCOL			0		// UNDERWATER PAR56 LED LAMP, 502266
 #define EFM8BB1_SUPPORT_WS_1200_PROTOCOL		0		// Alecto WS-1200 Series Wireless Weather Station
-#define EFM8BB1_SUPPORT_ALDI_4x_PROTOCOL		1		// ALDI Remote controlled wall sockets, 4x
-#define EFM8BB1_SUPPORT_SP45_PROTOCOL			1		// Meteo SPxx -  Weather station (PHU Metrex)
-#define EFM8BB1_SUPPORT_DC90_PROTOCOL			1		// Dooya DC90 remote
-#define EFM8BB1_SUPPORT_DG_HOSA_PROTOCOL		1		// Digoo DG-HOSA Smart 433MHz Wireless Household Carbon Monoxide Sensor
+#define EFM8BB1_SUPPORT_ALDI_4x_PROTOCOL		0		// ALDI Remote controlled wall sockets, 4x
+#define EFM8BB1_SUPPORT_SP45_PROTOCOL			0		// Meteo SPxx -  Weather station (PHU Metrex)
+#define EFM8BB1_SUPPORT_DC90_PROTOCOL			0		// Dooya DC90 remote
+#define EFM8BB1_SUPPORT_DG_HOSA_PROTOCOL		0		// Digoo DG-HOSA Smart 433MHz Wireless Household Carbon Monoxide Sensor
 #define EFM8BB1_SUPPORT_HT12a_PROTOCOL			0		// HT12A/HT12E chips - Generic Doorbell
 #define EFM8BB1_SUPPORT_HT12_Atag_PROTOCOL		0		// HT12A/HT12E chips - Atag Extractor - Plus/Minus/Lights/Timer
-#define EFM8BB1_SUPPORT_Kaku_PROTOCOL			1		// KaKu wall sockets
-#define EFM8BB1_SUPPORT_DIO_PROTOCOL			1		// DIO Chacon RF 433Mhz, Issue #95
-#define EFM8BB1_SUPPORT_1BYONE_PROTOCOL			1		// 1ByOne Doorbell, PR #97
-#define EFM8BB1_SUPPORT_Prologue_PROTOCOL		1		// Prologue Sensor, Issue #96
-#define EFM8BB1_SUPPORT_DOG_COLLAR_PROTOCOL		1		// Generic dog training collar - board label T-187-n (TX)-1, PR #100
-#define EFM8BB1_SUPPORT_BY302_PROTOCOL			1		// Byron BY302 Doorbell, Issue #102
-#define EFM8BB1_SUPPORT_DT_5514_PROTOCOL		1		// 5514 SILENT Dual Tech, Issue #104
-#define EFM8BB1_SUPPORT_H13726_PROTOCOL			1		// Auriol H13726 Weather Station, Issue #106
+#define EFM8BB1_SUPPORT_Kaku_PROTOCOL			0		// KaKu wall sockets
+#define EFM8BB1_SUPPORT_DIO_PROTOCOL			0		// DIO Chacon RF 433Mhz, Issue #95
+#define EFM8BB1_SUPPORT_1BYONE_PROTOCOL			0		// 1ByOne Doorbell, PR #97
+#define EFM8BB1_SUPPORT_Prologue_PROTOCOL		0		// Prologue Sensor, Issue #96
+#define EFM8BB1_SUPPORT_DOG_COLLAR_PROTOCOL		0		// Generic dog training collar - board label T-187-n (TX)-1, PR #100
+#define EFM8BB1_SUPPORT_BY302_PROTOCOL			0		// Byron BY302 Doorbell, Issue #102
+#define EFM8BB1_SUPPORT_DT_5514_PROTOCOL		0		// 5514 SILENT Dual Tech, Issue #104
+#define EFM8BB1_SUPPORT_H13726_PROTOCOL			0		// Auriol H13726 Weather Station, Issue #106
